79746677117bbd3f6978be6e54bf3793d4147d8d
Gild-Shogi <ok2336.coffee@gmail.com>
2022-10-25 10:33:15 +0900

    insert intern-taikenki

--- src/MyHistory/His2022.tsx
+++ src/MyHistory/His2022.tsx
@@ -1,5 +1,6 @@
 import {ChakraProvider, Stack} from '@chakra-ui/react'
-import {Heading,  Box, Tag, Center, HStack, VStack, Flex, Grid, Button, Divider} from '@chakra-ui/react';
+import {Heading,  Box, Tag, Center, HStack, VStack, Flex, Grid, Button, Divider, Link} from '@chakra-ui/react';
+import {ExternalLinkIcon} from '@chakra-ui/icons'
 
 function His2022(){
     return(
@@ -9,7 +10,7 @@ function His2022(){
         <Box fontSize="lg" pl="40px" pb="5px">国立 豊田高等専門学校 情報工学科 卒業</Box>
         <Box fontSize="lg" pl="40px" pb="5px">国立 筑波大学 情報学群 知識情報・図書館学類 知識情報システム主専攻 編入学</Box>
         <Box fontSize="lg" pl="40px" pb="5px">二子玉川 夏の陣2022 楽天エンジニアインターン(R-Hack) 新規プロトタイプ開発 3days 参加</Box>
-        <Box fontSize="lg" pl="40px" pb="5px">Supershipテックインターンシップ2022 プロダクトコース 参加</Box>
+        <Box fontSize="lg" pl="40px" pb="5px">Supershipテックインターンシップ2022 プロダクトコース 参加：<Link color="blue.400" href='https://note.com/gild_coffee/n/n2c964c8b4e2f' isExternal>体験記<ExternalLinkIcon /></Link></Box>
         </Stack>
     )
 }
